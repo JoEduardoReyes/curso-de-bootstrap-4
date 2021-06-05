cd18d458973b5f0c8bab3eb0979dfebc93ccad12
Eduardo Reyes <eduarno.1@hotmail.com>
2021-06-05 11:38:54 -0600

    Agregamos un modal de compra

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+// Tooltips
+
 var tooltipTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="tooltip"]')
 );
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+// modals
+var myModal = document.getElementById("myModal");
+var myInput = document.getElementById("myInput");
+
+myModal.addEventListener("shown.bs.modal", function () {
+  myInput.focus();
 });
